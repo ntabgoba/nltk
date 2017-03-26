b4a251b4e6763516e157d43927f4eeb63829ef55
ntabgoba <ntabgoba@gmail.com>
2017-03-26 14:49:51 +0900

    nltk defs

--- ls1.py
+++ ls1.py
@@ -29,3 +29,31 @@ text3.common_contexts(["life","God"])
 
 #dispersion plot -positional information of a word in a text
 text4.dispersion_plot(["citizens","democracy","freedom","duties","America"])
+
+#generate random text
+text2.generate()
+
+#token - technical name for a sequence of characters eg hair,:,his, that we want to treat as a group
+len(text3) #number of words in entire text
+
+#tokens in "to be or not to be" are "2to, 2be,1or,1not"
+#However, a set of above tokens, all duplicates wil be collapsed/removed
+
+len(sorted(set(text3)))
+# 2789 word types, - form or spelling of word independent of its specific occurences in a text
+
+#to calculate lexical richness of the text
+from __future__ import division
+len(text3)/len(set(text3))
+#16.05, each word is used 16 times on average.
+
+#how often a word occurs in a text
+text3.count("smote")
+text5.count("lol")
+100*text5.count("lol")/len(text5)
+
+#write a function that does the above
+def lexical_diversity(text):
+    return len(text)/len(set(text))
+def percentage(count, total):
+    return 100*count/total
