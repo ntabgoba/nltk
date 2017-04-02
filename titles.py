4082dd6fd4d87c2eea817acaafec96dc694d98dc
ntabgoba <ntabgoba@gmail.com>
2017-04-03 01:16:01 +0900

    fighting

--- titles.py
+++ titles.py
@@ -11,28 +11,40 @@ import re
 import matplotlib as plt
 os.getcwd()
 os.chdir('/Users/ntabgoba/nltk1/')
-titles = pd.read_csv('titles_eng.csv', sep='\t', header = 0)
-titles.head()
-titles.describe()
-titles.tail()
+df = pd.read_csv('titles_eng.csv', sep='\t', header = 0)
+df.head()
+df.describe()
+df.tail()
 from IPython.display import HTML
-HTML(titles.to_html())    #R's View like in html
+HTML(df.to_html())    #R's View like in html
 
 # How many authors
-titles.columns #show columns
-titles.shape  #(152115, 3)
-len(titles.author.unique()) #2047 Authors
-len(titles.title.unique())  #107739 Articles
-# Map authors to their titles in a dictionary
+df.columns #show columns
+df.shape  #(152115, 3)
+len(df.author.unique()) #2047 Authors
+len(df.title.unique())  #107739 Articles
+df.author.describe() #top 村田　正幸, #freq  972
 
+#replace Japanese names with numbers! or learn to handle jp and eng
+df['author'] = np.where(df['author'] == japense, df['sentence_id']),df['author'])
 
-# Do status on authors
+# Map authors to their df in a dictionary
+corpus =  dict(zip(df.author,df.title)) 
+type(corpus)
+corpus.items()
+
+                 
+# Do statss on authors
 
 #Normalize text and tokenize
 
-#remove stop words 
+#Remove stop words 
 
 #Do stats on article's words like Frequency and CumFreq
 
+#Stemming and Lemmatization of titles
 
 #Goal: -> Search and get closely related articles ??
+#-if author is search, bring their other articles or his/her co-authors
+#-titles -bring similar words
+#-
